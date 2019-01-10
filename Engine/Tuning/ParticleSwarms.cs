cabacf8c1d65012aaf36774bc834148adaced3f6
Erik Madsen <ekmadsen@comcast.net>
2019-01-09 23:49:04 -0600

    Updated file headers.

--- Engine/Tuning/ParticleSwarms.cs
+++ Engine/Tuning/ParticleSwarms.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections.Generic;
+﻿// +------------------------------------------------------------------------------+
+// |                                                                              |
+// |     MadChess is developed by Erik Madsen.  Copyright 2019.                   |
+// |     MadChess is free software.  It is distributed under the GNU General      |
+// |     Public License Version 3 (GPLv3).  See LICENSE file for details.         |
+// |     See https://www.madchess.net/ for user and developer guides.             |
+// |                                                                              |
+// +------------------------------------------------------------------------------+
+
+
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
