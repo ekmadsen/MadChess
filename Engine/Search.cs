e98daf7362198ce0f52ac385cbb0361aada558c3
Erik Madsen <ekmadsen@comcast.net>
2019-10-23 18:58:16 -0500

    2354 +/- 12 ELO at bullet chess.

--- Engine/Search.cs
+++ Engine/Search.cs
@@ -52,7 +52,7 @@ namespace ErikTheCoder.MadChess.Engine
         private const int _nullMoveReduction = 3;
         private const int _estimateBestMoveReduction = 2;
         private const int _pvsMinToHorizon = 3;
-        private const int _historyPriorMovePer128 = 64;
+        private const int _historyPriorMovePer128 = 256;
         private const int _quietSearchMaxFromHorizon = 3;
         private static MovePriorityComparer _movePriorityComparer;
         private static MoveScoreComparer _moveScoreComparer;
