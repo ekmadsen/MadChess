4012214e72e82de7ca17fc524354e9b573bee39f
Erik Madsen <46198609+ekmadsen@users.noreply.github.com>
2021-12-27 13:49:22 -0600

    Feature/update license file headers (#28)
    * Deleted GPLv3 license file.
    
    * Delete LICENSE
    
    Deleted LICENSE file from Git repository.  Previous commit deleted LICENSE file from the Visual Studio solution.
    
    * Create LICENSE
    
    Added MIT license file.
    
    * Rename LICENSE to LICENSE.md
    
    Added .md extension to license file.
    
    * Updated file headers to reference copyright from 2012 - 2022 and new MIT license.

--- Core/Delegates.cs
+++ Core/Delegates.cs
@@ -1,4 +1,14 @@
-﻿using ErikTheCoder.MadChess.Core.Game;
+﻿// +---------------------------------------------------------------------------+
+// |                                                                           |
+// |       MadChess is developed by Erik Madsen.  Copyright 2012 - 2022.       |
+// |       MadChess is free software.  It is distributed under the MIT         |
+// |       license.  See LICENSE.md file for details.                          |
+// |       See https://www.madchess.net/ for user and developer guides.        |
+// |                                                                           |
+// +---------------------------------------------------------------------------+
+
+
+using ErikTheCoder.MadChess.Core.Game;
 
 
 namespace ErikTheCoder.MadChess.Core;
--- Core/Utilities/ExtensionMethods.cs
+++ Core/Utilities/ExtensionMethods.cs
@@ -1,4 +1,14 @@
-﻿using System.Runtime.CompilerServices;
+﻿// +---------------------------------------------------------------------------+
+// |                                                                           |
+// |       MadChess is developed by Erik Madsen.  Copyright 2012 - 2022.       |
+// |       MadChess is free software.  It is distributed under the MIT         |
+// |       license.  See LICENSE.md file for details.                          |
+// |       See https://www.madchess.net/ for user and developer guides.        |
+// |                                                                           |
+// +---------------------------------------------------------------------------+
+
+
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 
 
