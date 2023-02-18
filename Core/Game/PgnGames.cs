91d3e7d1622cc4ba7b207e5c1fd9fa8a150bf340
Erik Madsen <ekmadsen@comcast.net>
2023-02-18 14:11:25 -0600

    Updated copyright message to include year 2023.

--- Core/Game/PgnGames.cs
+++ Core/Game/PgnGames.cs
@@ -1,6 +1,6 @@
 ﻿// +---------------------------------------------------------------------------+
 // |                                                                           |
-// |       MadChess is developed by Erik Madsen.  Copyright 2012 - 2022.       |
+// |       MadChess is developed by Erik Madsen.  Copyright 2012 - 2023.       |
 // |       MadChess is free software.  It is distributed under the MIT         |
 // |       license.  See LICENSE.md file for details.                          |
 // |       See https://www.madchess.net/ for user and developer guides.        |
