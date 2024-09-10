c9211fc05aaf60cc8c620e52ee2b6620597872d5
Erik Madsen <ekmadsen@comcast.net>
2024-09-10 07:13:38 -0500

    2768 +/- 10 Elo at bullet chess.

--- src/Engine/Intelligence/Search.cs
+++ src/Engine/Intelligence/Search.cs
@@ -48,13 +48,13 @@ public sealed class Search : IDisposable
     public bool Continue;
 
     private const int _nullMoveReduction = 3;
-    private const int _nullStaticScoreReduction = 150;
+    private const int _nullStaticScoreReduction = 180;
     private const int _nullStaticScoreMaxReduction = 4;
     private const int _iidReduction = 2;
     private const int _lmrMaxIndex = 64;
-    private const int _lmrScalePer128 = 54;
-    private const int _lmrConstPer128 = -128;
-    private const int _recapturesOnlyMaxFromHorizon = 7;
+    private const int _lmrScalePer128 = 56;
+    private const int _lmrConstPer128 = -144;
+    private const int _recapturesOnlyMaxFromHorizon = 3;
 
     private readonly LimitStrengthSearchConfig _limitStrengthConfig;
     private readonly int[] _limitStrengthElos;
